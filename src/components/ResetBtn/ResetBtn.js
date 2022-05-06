9c7e9a4c0f6c4c4501560b5af4e3107e11185c75
AVKostin <alexander.kostin.ua@gmail.com>
2022-05-06 13:48:46 +0300

    Create ResetBtn.js

---
+++ src/components/ResetBtn/ResetBtn.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import countTotalFeedback from '../App';
+import './ResetBtn.css';
+
+class ResetBtn extends Component {
+  state = {
+    visible: false,
+  };
+
+  toggle = () => {
+    this.setState(prevState => ({
+      visible: !prevState.visible,
+    }));
+  };
+
+  render() {
+    const { state } = this.state;
+    this.setState();
+    return (
+      <div className="ResetBtn">
+        <button
+          type="button"
+          // className="Dropdown__toggle"
+          onClick={() => {
+            countTotalFeedback(state);
+          }}
+        >
+          {state ? '' : 'Ресет'}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ResetBtn;
