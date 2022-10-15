65d97a63266dfad2670214ebc95f1bb86d3e399f
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-15 20:20:09 +0300

    refactoring and cleraring code

--- src/components/ResetBtn/ResetBtn.js
+++ src/components/ResetBtn/ResetBtn.js
@@ -1,33 +1,30 @@
-import React, { Component } from 'react';
-import countTotalFeedback from '../App';
+import React, { Component } from "react";
+import countTotalFeedback from "../App";
 
 class ResetBtn extends Component {
-  state = {
-    visible: false,
-  };
-
-  toggle = () => {
-    this.setState(prevState => ({
-      visible: !prevState.visible,
-    }));
-  };
-
-  render() {
-    const { state } = this.state;
-    this.setState();
-    return (
-      <div className="ResetBtn">
-        <button
-          type="button"
-          onClick={() => {
-            countTotalFeedback(state);
-          }}
-        >
-          {state ? '' : 'Ресет'}
-        </button>
-      </div>
-    );
-  }
+    state = {
+        visible: false,
+    };
+    toggle = () => {
+        this.setState((prevState) => ({
+            visible: !prevState.visible,
+        }));
+    };
+    render() {
+        const { state } = this.state;
+        this.setState();
+        return (
+            <div className="ResetBtn">
+                <button
+                    type="button"
+                    onClick={() => {
+                        countTotalFeedback(state);
+                    }}
+                >
+                    {state ? "" : "Ресет"}
+                </button>
+            </div>
+        );
+    }
 }
-
 export default ResetBtn;
