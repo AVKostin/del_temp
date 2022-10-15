b4fb5126926048971a5824a2902c3d58b1848d76
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-15 20:19:45 +0300

    refactoring and cleraring code

--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -1,17 +1,16 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
 export const StatisticsList = styled.ul`
-   padding: 5px;
-  `;
+  padding: 5px;
+`;
 
 export const StatisticsItem = styled.li`
   display: flex;
   align-items: center;
-  &:not(:last-child) {
-    margin-bottom: 20px;
+    &:not(:last-child) {
+        margin-bottom: 20px;
   }
-
-  & span {
-    margin-left: 20px;
+    & span {
+        margin-left: 20px;
   }
 `;
