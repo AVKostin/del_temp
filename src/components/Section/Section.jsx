1b3265a240298b419dee7dffedd83f661f05c7dd
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-15 20:20:04 +0300

    refactoring and cleraring code

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,16 +1,16 @@
-import PropsType from 'prop-types';
-import { Container, Title } from './Section.styled';
+import PropsType from "prop-types";
+import { Container, Title } from "./Section.styled";
+
 const Section = ({ title, children }) => {
-  return (
-    <Container>
-      {title && <Title>{title}</Title>}
-      {children}
-    </Container>
-  );
+    return (
+        <Container>
+            {title && <Title>{title}</Title>}
+            {children}
+        </Container>
+    );
 };
 
 Section.propTypes = {
-  title: PropsType.string,
+    title: PropsType.string,
 };
-
 export default Section;
