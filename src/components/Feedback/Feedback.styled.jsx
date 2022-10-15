4dab366749ebcc86510ade17d40d737057a2303f
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-15 20:20:24 +0300

    refactoring and cleraring code

--- src/components/Feedback/Feedback.styled.jsx
+++ src/components/Feedback/Feedback.styled.jsx
@@ -5,39 +5,36 @@ export const FeedbackWrapper = styled.div`
 `;
 
 export const FeedbackButton = styled.button`
-  &:not(:last-child) {
-    margin-right: 10px;
+    &:not(:last-child) {
+      margin-right: 10px;
   }
   width: 110px;
   height: 30px;
   border: 1px solid #030e1845;
   border-radius: 4px;
   transform: scale(1);
-
   transform 250ms cubic-bezier(0.4, 0, 0.2, 1),
   color 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
-  &:nth-of-type(1):hover,
-  &:nth-of-type(1):focus {
-    font-weight: 700;
-    transform: scale(1.05);
-    background-color: #0045e1;
-    color: #66df4edc;
+    &:nth-of-type(1):hover,
+    &:nth-of-type(1):focus {
+      font-weight: 700;
+      transform: scale(1.05);
+      background-color: #0045e1;
+      color: #66df4edc;
   }
-
-  &:nth-of-type(2):hover,
-  &:nth-of-type(2):focus {
-    font-weight: 700;
-    transform: scale(1.05);
-    background-color: #0045e1;
-    color: #93a790dc;
+    &:nth-of-type(2):hover,
+    &:nth-of-type(2):focus {
+      font-weight: 700;
+      transform: scale(1.05);
+      background-color: #0045e1;
+      color: #93a790dc;
   }
-
-  &:nth-of-type(3):hover,
-  &:nth-of-type(3):focus {
-    font-weight: 700;
-    transform: scale(1.05);
-    background-color: #0045e1;
-    color: #f10b0bdf;
+    &:nth-of-type(3):hover,
+    &:nth-of-type(3):focus {
+      font-weight: 700;
+      transform: scale(1.05);
+      background-color: #0045e1;
+      color: #f10b0bdf;
   }
 `;
