81e4c2598a19e4573f2bf0bba75892a74ebb28c0
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-15 20:20:28 +0300

    refactoring and cleraring code

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,24 +1,23 @@
-import PropTypes from 'prop-types';
-import { FeedbackWrapper, FeedbackButton } from './Feedback.styled';
+import PropTypes from "prop-types";
+import { FeedbackWrapper, FeedbackButton } from "./Feedback.styled";
 
 const Feedback = ({ options, onLeaveFeedback }) => (
-  <FeedbackWrapper>
-    {options.map((option, index) => (
-      <FeedbackButton
-        key={index}
-        name={option}
-        type="button"
-        onClick={onLeaveFeedback}
-      >
-        {option}
-      </FeedbackButton>
-    ))}
-  </FeedbackWrapper>
+    <FeedbackWrapper>
+        {options.map((option, index) => (
+            <FeedbackButton
+                key={index}
+                name={option}
+                type="button"
+                onClick={onLeaveFeedback}
+            >
+                {option}
+            </FeedbackButton>
+        ))}
+    </FeedbackWrapper>
 );
 
 Feedback.propTypes = {
-  options: PropTypes.array.isRequired,
-  onLeaveFeedback: PropTypes.func.isRequired,
+    options: PropTypes.array.isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired,
 };
-
 export default Feedback;
