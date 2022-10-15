f611b511bb1b5b2420d29d424503f1fdf47bcf4b
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-15 20:20:20 +0300

    refactoring and cleraring code

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,16 +1,15 @@
-import PropsType from 'prop-types';
-import { NotificationText } from './Notification.styled';
+import PropsType from "prop-types";
+import { NotificationText } from "./Notification.styled";
 
 const Notification = ({ message }) => {
-  return (
-    <NotificationText>
-      <span>{message}</span>
-    </NotificationText>
-  );
+    return (
+        <NotificationText>
+            <span>{message}</span>
+        </NotificationText>
+    );
 };
 
 Notification.propsType = {
-  message: PropsType.string.isRequired,
+    message: PropsType.string.isRequired,
 };
-
 export default Notification;
